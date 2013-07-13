58e42714758cb11fc3d8a150a4c70d63c46610eb
Himanshu Sharma <himanshu.sharma@makepositive.com>
2013-07-13 13:49:32 +0530

    ch

--- public/javascripts/functions.js
+++ public/javascripts/functions.js
@@ -2,7 +2,7 @@ $(document).change("select#flip-1",function() {
     value = $("#flip-1").val();
     if(value== "on"){
     	$.get('/turnOn', function(){
-				});)
+				});
     }
     else {
     	$.get('/turnOff', function(){
