835138f68a487b16f737c6f046afee7e7805ba0d
Himanshu Sharma <himanshu.sharma@makepositive.com>
2013-07-13 14:43:11 +0530

    4 leds

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.configure(function(){
   app.set('port', process.env.PORT || 3000);
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
-  app.use(express.favicon());
+  //app.use(express.favicon());
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
   app.use(express.methodOverride());
@@ -31,8 +31,8 @@ app.configure('development', function(){
 
 app.get('/', routes.index);
 app.get('/blink', servo.blink);
-app.get('/turnOn', servo.turnOn);
-app.get('/turnOff', servo.turnOff);
+app.get('/turnOn/', servo.turnOn);
+app.get('/turnOff/', servo.turnOff);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
--- public/javascripts/functions.js
+++ public/javascripts/functions.js
@@ -1,11 +1,50 @@
 $(document).change("select#flip-1",function() {
     value = $("#flip-1").val();
     if(value== "on"){
-    	$.get('/turnOn', function(){
+    	$.get('/turnOn/?pin=7', function(){
 				});
     }
     else {
-    	$.get('/turnOff', function(){
+    	$.get('/turnOff/?pin=7', function(){
+				});
+    }
+
+	});
+
+$(document).change("select#flip-2",function() {
+    value = $("#flip-1").val();
+    if(value== "on"){
+    	$.get('/turnOn/?pin=11', function(){
+				});
+    }
+    else {
+    	$.get('/turnOff/?pin=11', function(){
+				});
+    }
+
+	});
+
+$(document).change("select#flip-3",function() {
+    value = $("#flip-1").val();
+    if(value== "on"){
+    	$.get('/turnOn/?pin=8', function(){
+				});
+    }
+    else {
+    	$.get('/turnOff/?pin=8', function(){
+				});
+    }
+
+	});
+
+$(document).change("select#flip-4",function() {
+    value = $("#flip-1").val();
+    if(value== "on"){
+    	$.get('/turnOn/?pin=12', function(){
+				});
+    }
+    else {
+    	$.get('/turnOff/?pin=12', function(){
 				});
     }
 
